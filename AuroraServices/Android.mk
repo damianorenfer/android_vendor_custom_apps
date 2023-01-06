4492f81ce28da1034088ac12aa31722d197559c8
Damiano Renfer <x9w2n7xnu@relay.firefox.com>
2023-01-06 10:38:04 +0100

    Add Android 13 support
    Also update AuroraStore to 4.1.1

--- AuroraServices/Android.mk
+++ AuroraServices/Android.mk
@@ -2,14 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := permissions_com.aurora.services.xml
-LOCAL_MODULE_TAGS := optional
+LOCAL_PRODUCT_MODULE := true
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT_ETC)/permissions
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
+LOCAL_PRODUCT_MODULE := true
 LOCAL_MODULE := AuroraServices
 LOCAL_SRC_FILES := AuroraServices_v1.1.1.apk
 LOCAL_MODULE_CLASS := APPS
